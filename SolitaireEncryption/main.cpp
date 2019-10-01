2ff6550b54dfffa93435edde7da3c01017e3b41e
GoldenGrape <goldengrape@goldengrape.org>
2019-10-01 10:23:03 +0800

    Initial Commit

---
+++ SolitaireEncryption/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SolitaireEncryption
+//
+//  Created by GoldenGrape on 2019/10/1.
+//  Copyright © 2019 goldengrape. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
